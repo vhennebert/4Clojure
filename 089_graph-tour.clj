faea08e18afc72e07cec7b7d5d61dad6f2ad95ff
Vincent Hennebert <vhennebert@users.noreply.github.com>
2014-07-17 10:48:22 +0200

    89. Graph Tour

---
+++ 089_graph-tour.clj
@@ -0,0 +1,11 @@
+(fn [e]
+  (and
+    (loop [[x & v] (first e) c #{} e e]
+      (cond
+        x (let [c (conj c x)
+                {conn x, disconn nil} (group-by #(some #{x} %) e)
+                n (remove c (flatten conn))]
+            (recur (concat v n) c disconn))
+        (seq e) false
+        :else true))
+    (> 3 (->> e flatten frequencies vals (filter odd?) count))))
